1d2cbf132bcffaba41d6d2977e25fdb49249a0c8
josemagno <43430330+magnoalh@users.noreply.github.com>
2021-08-23 23:28:57 -0300

    Create index.php
    treinando formulário com varias validações e detalhes técnicos.

---
+++ php-intro/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" >
+    <title>Formulário de Inscrição</title>
+    <meta name="author" content="" >
+    <meta name="description" content="" >
+    <meta name="viewport" content="width=device-width , initial-scale = 1" >
+</head>
+<body>
+<p> FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES </p>
+<form action="script.php" method="POST" >
+    <p>Seu Nome : <input type="text" name="nome" /></p>
+    <p>Sua Idade : <input type="text" name="idade" /></p>
+    <p><input type="submit" /></p>
+</form>
+</body>
+</html>
+
