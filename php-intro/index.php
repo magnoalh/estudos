13314a2891c1ff3c9675eaffc8cb62ec4090237d
josemagno <43430330+magnoalh@users.noreply.github.com>
2021-08-30 00:17:55 -0300

    Update index.php

--- php-intro/index.php
+++ php-intro/index.php
@@ -1,3 +1,7 @@
+<?php
+    session_start();
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,6 +14,18 @@
 <body>
 <p> FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES </p>
 <form action="script.php" method="POST" >
+    <?php
+        $mensagemDeSucesso = isset($_SESSION['mensagem-de-sucesso']) ? $_SESSION['mensagem-de-sucesso'] : '';
+        if(!empty($mensagemDeSucesso))
+        {
+            echo $mensagemDeSucesso;
+        }
+        $mensagemDeErro = isset($_SESSION['mensagem-de-erro']) ? $_SESSION['mensagem-de-erro'] : '';
+        if(!empty($mensagemDeErro))
+        {
+            echo $mensagemDeErro;
+        }
+    ?>
     <p>Seu Nome : <input type="text" name="nome" /></p>
     <p>Sua Idade : <input type="text" name="idade" /></p>
     <p><input type="submit" /></p>
